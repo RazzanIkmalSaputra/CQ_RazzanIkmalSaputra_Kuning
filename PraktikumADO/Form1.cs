9ffb93e21ccdebb0c3cb3a1bca30286ec0bd3b5c
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-03-13 13:54:38 +0700

    menambahkan btnUpdate

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -83,9 +83,28 @@ namespace PraktikumADO
 
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void btnUpdate_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string Query = "UPDATE Mahasiswa SET Alamat = 'Yohyakarta' WHERE NIM = '23110100001'";
+
+                cmd = new SqlCommand(Query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("jumlah baris terpengaruh :" + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
         }
     }
 }
